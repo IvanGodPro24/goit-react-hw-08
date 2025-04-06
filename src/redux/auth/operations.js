49d63f5eb487a81c5b6e06b3e0884cc7ed0adba8
IvanGodPro24 <vanyanep@gmail.com>
2025-04-06 17:39:42 +0300

    Change backend url

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 // axios.defaults.baseURL = "https://connections-api.goit.global";
-axios.defaults.baseURL = "http://localhost:9090";
+axios.defaults.baseURL = "https://blended-contacts-app.onrender.com";
 
 const setAuthHeader = (token) => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
